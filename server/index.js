5dd8f1a71068a56f20ee2397e83e99a2d7237f15
Adel-Alimov <adel.alimov@list.ru>
2026-09-05 17:21:22 +0400

    fix bugs

--- server/index.js
+++ server/index.js
@@ -15,7 +15,23 @@ const mongoDB = process.env.MONGO;
 const PORT = process.env.PORT || 3001;
 
 //Middleware
-app.use(cors());
+// Middleware
+app.use(
+    cors({
+        origin: ["https://posts-omega-six.vercel.app", "http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    }),
+);
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.static("uploads"));
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 app.use(fileUpload());
 app.use(express.json());
 app.use(express.static("uploads"));
