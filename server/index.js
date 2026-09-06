d4e815029d023007052e4d01cadc43df8c62ba48
Adel-Alimov <adel.alimov@list.ru>
2026-09-06 08:40:24 +0400

    fix img on other device

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use(fileUpload());
 app.use(express.json());
-app.use(express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/api/auth", authRoute);
